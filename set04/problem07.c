1af6d79115c7d2eb3fe09a7dd1313052947f771b
akshaya7483 <akshayas.bsc23@rvu.edu.in>
2023-12-09 14:37:07 +0530

    committed 12/9/2023, 2:37:06 PM

--- set04/problem07.c
+++ set04/problem07.c
@@ -1,27 +1,48 @@
 // 7. Write a program to add two fractions
-
-typedef struct {
-    int num, den;
-} Fraction;
-Fraction input_fraction()
+#include<stdio.h>
+void input(int *num1,int *den1,int *num2,int *den2)
 {
-    Fraction x;
     printf("enter the numerator :");
-    scanf("%f",&x.num);
+    scanf("%d",num1);
     printf("enter the denominator :");
-    scanf("%f",&x.den);
-    return x;
+    scanf("%d",den1);
+    printf("enter the numerator :");
+    scanf("%d",num2);
+    printf("enter the denominator :");
+    scanf("%d",den2);
 }
-int find_gcd(int a,int b)
+void add(int num1,int den1,int num2,int den2,int *res_num,int *res_den)
 {
+    *res_num=num1*den2+num2*den1;
+    *res_den=den1*den2;
+    int gcd=Gcd(*res_num,*res_den);
+    *res_num=*res_num/gcd;
+    *res_den=*res_den/gcd;
     
 }
-Fraction add_fractions(Fraction f1, Fraction f2);
-void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum);
+int Gcd(int res_num,int res_den)
+{
+    int gcd;
+    int i=1;
+    while(i<=res_num && i<=res_den)
+    {
+        if(res_num%i==0 && res_den%i==0)
+        {
+            gcd=i;
+        }
+        i++;
+    }
+    return gcd;
+}
+void output(int res_num,int res_den)
+{
+     printf("sum of fraction is %d/%d",res_num,res_den);
+}
 int main()
 {
-    Fraction a,b,sum;
-    a=input_fraction();
-    b=input_fraction();
-    find_gcd(a.num,b.num);
+    int num1,den1,num2,den2,res_num,res_den,gcd;
+    input(&num1,&den1,&num2,&den2);
+    add(num1,den1,num2,den2,&res_num,&res_den);
+    output(res_num,res_den);
+    return 0;
 }
