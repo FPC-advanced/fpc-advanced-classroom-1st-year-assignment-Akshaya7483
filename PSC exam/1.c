5f09f7b41b2debaf77c54326f21401a1738bce46
akshaya7483 <akshayas.bsc23@rvu.edu.in>
2023-12-19 16:06:59 +0530

    committed 12/19/2023, 4:06:58 PM

--- PSC exam/1.c
+++ PSC exam/1.c
@@ -1,42 +1,29 @@
 #include<stdio.h>
-int input()
-{
-	int x;
-	printf("enter the number :");
-	scanf("%d",&x);
-	return x;
+
+int input() {
+    int x;
+    printf("Enter the number: ");
+    scanf("%d", &x);
+    return x;
 }
-int find_fact(int n)
-{
-    int j=2;
-	for(int i=1;i!=n/2;i++)
-	{
-		j=j*i;
-	}
-    if(j==n)
-        {
-            return 1;
-        }
-        else{
-            return 0;
-        }
-}
-void output(int n,int fact)
-{
-    if(fact>0)
-    {
-        printf("it is a factorial number ");
+
+int is_factorial(int n) {
+    int i = 1, fact = 1;
+
+    while (fact < n) {
+        i++;
+        fact *= i;
     }
-    else
-    {
-        printf("it is not a factorial number ");
+
+    if (fact == n) {
+        return 1; // It is a factorial number
+    } else {
+        return 0; // It is not a factorial number
     }
 }
-int main()
-{
-	int n,fact;
-	n=input();
-	fact=find_fact(n);
-	output(n,fact);
-    return 0;
-}
+
+void output(int n, int fact) {
+    if (fact > 0) {
+        printf("%d is a factorial number.\n", n);
+    } else {
+        printf("%d is not a factorial n
