5836f29766b36d71f854c2990fa226829cfdac12
akshaya7483 <akshayas.bsc23@rvu.edu.in>
2023-12-19 16:07:59 +0530

    committed 12/19/2023, 4:07:58 PM

--- PSC exam/1.c
+++ PSC exam/1.c
@@ -27,6 +27,9 @@ int is_factorial(int n) {
 void output(int n, int fact) {
     if (fact > 0) 
     {
-        printf("%d is a factorial number.\n", n);
-    } else {
-        printf("%d is not a factorial n
+        printf("%d is a factorial number\n", n);
+    } 
+    else 
+    {
+        printf("%d is not a factorial number \n",n);
+    }
