b27d5f35ec8a8fabde984db1045c68d1d2d062f9
akshaya7483 <akshayas.bsc23@rvu.edu.in>
2023-12-19 12:09:05 +0530

    committed 12/19/2023, 12:09:04 PM

--- PSC exam/1.c
+++ PSC exam/1.c
@@ -8,13 +8,11 @@ int input()
 }
 int find_fact(int n)
 {
-    int j=n;
-    int i=2; 
-	while(j%2==0)
+    int j=n; 
+	for(int i=2;i<j;i++) //120 while(j%2==0)
 	{                                         
 		j=j/i; 
 	}
-    i++;
 	return j;
 }
 void output(int n,int fact)
