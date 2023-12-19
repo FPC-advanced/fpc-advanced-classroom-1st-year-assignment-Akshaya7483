ad1566ef7fb1e9ff37c3b8f3eeaa9662fc41b66b
akshaya7483 <akshayas.bsc23@rvu.edu.in>
2023-12-19 12:13:06 +0530

    committed 12/19/2023, 12:13:04 PM

--- PSC exam/3.c
+++ PSC exam/3.c
@@ -2,7 +2,9 @@
 int number_of_players()
 {
     int x;
-    printf("enter the number of players :")
+    printf("enter the number of players :");
+    scanf("%d",&x);
+    return x;
 }
 int main()
 {
