10ce26b286fe63eca614daa0d91847bb86980c73
akshaya7483 <akshayas.bsc23@rvu.edu.in>
2023-12-19 16:13:29 +0530

    committed 12/19/2023, 4:13:28 PM

---
+++ PSC exam/trail.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int input()
+{
+	int x;
+	printf("enter the number :");
+	scanf("%d",&x);
+	return x;
+}
+int find_fact(int n)
+{
+	int i=1;j=1;
+	while(j<n)
+	{
+		j*=i;
+		i++;
+	}
+	if(j==n)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+	
+}
+void output(int n,int fact)
+{
+	if(fact>0)
+	{
+		printf("%d is a factorial number \n",n);
+	}
+	else
+	{
+		printf("%d is not a factorial number \n",n);
+	}
+}
+int main()
+{
+	int n,fact;
+	n=input();
+	fact=find_fact(n);
+	output(n,fact);
+	return 0;
+}
