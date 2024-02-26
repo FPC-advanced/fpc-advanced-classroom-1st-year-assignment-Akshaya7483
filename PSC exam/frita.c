49c79aeca201f5bef7e33f6749db67869ced86ac
akshaya7483 <akshayas.bsc23@rvu.edu.in>
2024-02-26 23:03:48 +0530

    committed 2/26/2024, 11:03:47 PM

--- PSC exam/frita.c
+++ PSC exam/frita.c
@@ -96,98 +96,16 @@ void input_team_name(game *name,int m,int n)
        input_player_details(a,n);
     }
 }
-void input(game *,int n)
+void input(game *name,int n)
 {
     printf("enter the game name :\n");
     scanf("%s",name->game_name);
     input_team_name(name,n);
-}
-int convert_string(team a[],int i) 
-{
-    if (a[i].y.intrest[0]=='y' || a[i].y.intrest[0]=='Y')
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-} 
-int verify_height(team a[],int i)
-{
-    
-    if(a[i].y.height>=6)
-    {
-        return 1;
-    }
-    return 0;
-}
-fritacole attribute(team a[],int n)
-{
-    fritacole sum;
-    sum.integrity=0;
-    sum.discipline=0;
-    sum.height=0;
-    sum.ability=0;
-    sum.converted_intrest=0;
-    sum.attribute_point=0;
-    int i,x,z;
-    for(i=0;i<n;i++)
-    {
-        sum.integrity+=a[i].y.integrity;
-        sum.discipline+=a[i].y.discipline;
-        x=verify_height(a,i);
-        sum.height+=x;
-        sum.ability+=a[i].y.ability;
-        z=convert_string(a,i);
-        sum.converted_intrest+=z;
-
-    }
-    return sum;
-}
-fritacole points_scored(team a[],int n)
-{
-    int i;
-    fritacole sum;
-    sum.fouls=0;
-    sum.point=0;
-    for(i=0;i<n;i++)
-    {
-        sum.point+=a[i].y.point;
-        sum.fouls+=a[i].y.fouls;
-    }
-    return sum;
-}
-fritacole Score(team a[],int m,int n,team score[][2])
-{
-    int i,j=0;
-    for(i=0;i<m;i++)
-    {
-        score[i][j].y=points_scored(a,n);
-        score[i][j+1].y=attribute(a,n);
-    }
-}
-fritacole compare(team score[][2])
-{
-     int i,j=0;
-    for(i=0;i<2;i++)
-    {
-        if(score[i][j].y>score[i+][j].y)
-        {
-            return
-        }
-    }
 }
 int main()
 {
     int n,m=2;
     game name;
     n=get_number_of_players();
-    input(&name);
-    team a[m];
-    input_team_name(a,m,n);
-    team score[2][2];
-    Score(a,m,n,score);
-    compare(&score);
    return 0;
 }
